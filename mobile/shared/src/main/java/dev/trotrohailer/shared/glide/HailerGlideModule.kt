9aed08203f413c2b447964888b9d626401e2f932
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-08-25 13:42:28 +0000

    Update HailerGlideModule.kt

--- mobile/shared/src/main/java/dev/trotrohailer/shared/glide/HailerGlideModule.kt
+++ mobile/shared/src/main/java/dev/trotrohailer/shared/glide/HailerGlideModule.kt
@@ -1,8 +1,24 @@
 package dev.trotrohailer.shared.glide
 
+import android.net.Uri
+import android.widget.ImageView
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.module.AppGlideModule
+import dev.trotrohailer.shared.R
 
 @GlideModule
 open class HailerGlideModule : AppGlideModule()
 
+fun ImageView.load(uri: Uri?, circleCrop: Boolean = true) = GlideApp.with(context)
+    .asBitmap()
+    .load(uri.toString())
+    .apply {
+        if (circleCrop) {
+            circleCrop()
+                .placeholder(R.drawable.avatar_placeholder)
+                .error(R.drawable.avatar_placeholder)
+        }
+    }
+    .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+    .into(this)
