66af0182f4219c825404165b3e4e6dd2892650f8
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-08-25 10:43:33 +0000

    Build succeeded

---
+++ mobile/shared/src/main/java/dev/trotrohailer/shared/glide/HailerGlideModule.kt
@@ -0,0 +1,8 @@
+package dev.trotrohailer.shared.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class HailerGlideModule : AppGlideModule()
+
